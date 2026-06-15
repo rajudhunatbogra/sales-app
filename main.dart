6b9a7d441d8c65c47b74fdadda80e624da64f9f8
rajudhunatbogra <45766835+rajudhunatbogra@users.noreply.github.com>
2026-06-15 20:02:50 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'database_helper.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -772,5 +773,37 @@ class _SalesPageState extends State<SalesPage> {
         ),
       ),
     );
+  }
+}
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('sales_memo.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: (db, version) async {
+      await db.execute('''
+        CREATE TABLE memos (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT,
+          address TEXT,
+          totalBill TEXT,
+          date TEXT
+        )
+      ''');
+    });
+  }
+
+  Future<int> insertMemo(Map<String, dynamic> row) async {
+    final db = await instance.database;
+    return await db.insert('memos', row);
   }
 }
