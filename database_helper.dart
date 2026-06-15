5389c1422aa772790d250bab3bebfc60b48a3c8b
rajudhunatbogra <45766835+rajudhunatbogra@users.noreply.github.com>
2026-06-15 18:46:08 +0600

    Create database_helper.dart

---
+++ database_helper.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('sales_memo.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE memos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        address TEXT,
+        totalBill TEXT,
+        date TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertMemo(Map<String, dynamic> row) async {
+    final db = await instance.database;
+    return await db.insert('memos', row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllMemos() async {
+    final db = await instance.database;
+    return await db.query('memos', orderBy: 'id DESC');
+  }
+}
