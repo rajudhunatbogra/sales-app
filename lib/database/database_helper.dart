155d52b6b055ced3f4e0d0138b1f79915f62ed8f
rajudhunatbogra <45766835+rajudhunatbogra@users.noreply.github.com>
2026-06-16 00:13:46 +0600

    Update database_helper.dart

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,1 +1,45 @@
-class DatabaseHelper {}
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDB('sales_app.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+CREATE TABLE memos (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+sl TEXT,
+customerName TEXT,
+phone TEXT,
+address TEXT,
+date TEXT,
+totalBill REAL,
+dueAmount REAL,
+paymentStatus TEXT
+)
+''');
+  }
+}
