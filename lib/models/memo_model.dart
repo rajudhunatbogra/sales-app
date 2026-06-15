f3985d5c19ede2537e0a36338755e556fb97bf88
rajudhunatbogra <45766835+rajudhunatbogra@users.noreply.github.com>
2026-06-16 00:08:32 +0600

    Update memo_model.dart

--- lib/models/memo_model.dart
+++ lib/models/memo_model.dart
@@ -1,1 +1,47 @@
-class MemoModel {}
+class MemoModel {
+  String sl;
+  String customerName;
+  String phone;
+  String address;
+  String date;
+  double totalBill;
+  double dueAmount;
+  String paymentStatus;
+
+  MemoModel({
+    required this.sl,
+    required this.customerName,
+    required this.phone,
+    required this.address,
+    required this.date,
+    required this.totalBill,
+    required this.dueAmount,
+    required this.paymentStatus,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'sl': sl,
+      'customerName': customerName,
+      'phone': phone,
+      'address': address,
+      'date': date,
+      'totalBill': totalBill,
+      'dueAmount': dueAmount,
+      'paymentStatus': paymentStatus,
+    };
+  }
+
+  factory MemoModel.fromMap(Map<String, dynamic> map) {
+    return MemoModel(
+      sl: map['sl'] ?? '',
+      customerName: map['customerName'] ?? '',
+      phone: map['phone'] ?? '',
+      address: map['address'] ?? '',
+      date: map['date'] ?? '',
+      totalBill: (map['totalBill'] ?? 0).toDouble(),
+      dueAmount: (map['dueAmount'] ?? 0).toDouble(),
+      paymentStatus: map['paymentStatus'] ?? '',
+    );
+  }
+}
