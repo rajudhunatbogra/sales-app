c115516170b7a01d722c9791621a3ee5d9155f74
rajudhunatbogra <45766835+rajudhunatbogra@users.noreply.github.com>
2026-06-16 15:48:53 +0600

    Update storage_service.dart

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,1 +1,15 @@
-class StorageService {}
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageService {
+  static const String _memoKey = 'jewelry_last_memo_no';
+
+  static Future<int> getLastMemoNumber() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_memoKey) ?? 2000; 
+  }
+
+  static Future<void> incrementMemoNumber(int currentNumber) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_memoKey, currentNumber + 1);
+  }
+}
